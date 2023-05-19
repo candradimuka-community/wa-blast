3841a553afdcdb75eb07298a80bdc011b62ceb9c
aronei44 <meliodasmeliodas224@gmail.com>
2023-05-19 20:07:03 +0700

    feat (time now)

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pywhatkit
+from datetime import datetime, timedelta
 
-def send_msg(no):
-    pywhatkit.sendwhatmsg("+6282216557939","testing dari wa blast",10,49)
+def send_msg(no, time_now):
+
+    current_time = time_now.strftime("%H:%M").split(':')
+    name = no['name']
+    pywhatkit.sendwhatmsg(no['number'],f"testing dari wa blast untuk: {name}",current_time[0],current_time[1])
+
+
 
 if __name__=="__main__":
-    send_msg(1)
+    data = [
+        {
+            "number": "+6281210693426",
+            "name":"nuhinahinu"
+        }
+    ]
+    for d in data: 
+        send_msg(d,datetime.now() + timedelta(minutes=2))
